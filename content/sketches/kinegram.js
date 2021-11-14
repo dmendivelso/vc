1f3f021f4cd2d7028600794a3be99fec3b47140d
Juan Pablo Carmona <jucarmonam@unal.edu.co>
2021-11-13 20:28:38 -0500

    adelanto

--- content/sketches/kinegram.js
+++ content/sketches/kinegram.js
@@ -1,20 +1,57 @@
 new p5((p) => {
   let img; // Declare variable 'img'.
+  let lines = [];
 
   p.setup = function () {
     p.createCanvas(800, 350);
     img = p.loadImage("/vc/sketches/pacman.png"); // Load the image
     p.strokeWeight(14); // medium weight lines
+
+    var step = 15;
+    let i = 0;
+    //vertical lines
+    for (var x = step; x < p.width; x = x + step) {
+      lines[i] = new Line(p.mouseX + x, 0, p.mouseX + x, p.height);
+      i++;
+    }
+
+    p.button = p.createButton("animation");
+    p.button.position(img.x + img.width, 65);
+    p.button.mousePressed(p.greet);
   };
 
   p.draw = function () {
     // Displays the image at its actual size at point (0,0)
     p.image(img, 0, 0, img.width + img.width / 4, img.height + img.height / 4);
 
-    var step = 15;
-    //vertical lines
-    for (var x = step; x < p.width; x = x + step) {
-      p.line(p.mouseX + x, 0, p.mouseX + x, p.height);
+    for (var i = 0; i < lines.length; i++) {
+      lines[i].display();
     }
   };
+
+  p.greet = function () {
+    p.erase();
+    var step = 15;
+    for (var x = step; x < p.width; x = x + step) {
+      p.line(1 + x, 0, 1 + x, p.height);
+    }
+  };
+
+  // Jitter class
+  class Line {
+    constructor(x1, y1, x2, y2) {
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+    }
+
+    move(movex) {
+      this.x += movex;
+    }
+
+    display() {
+      p.line(this.x1, this.y1, this.x2, this.y2);
+    }
+  }
 }, "kinegram");
